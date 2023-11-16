a31f63defe6b171793535308b1b69b1db1676f21
Valerie Duffey <69978259+valerieduffey@users.noreply.github.com>
2023-11-16 15:48:19 -0500

    font

--- WebApp/Client/src/Header.js
+++ WebApp/Client/src/Header.js
@@ -48,7 +48,7 @@ function Header() {
       <header className="header">
         <nav>
           <ul className="right">
-            <div className="left">WhatToCook</div>
+            <div style={{font:'cursive'}}className="left">WhatToCook</div>
             <Home/>
             <Recipe/>
             <Price/>
