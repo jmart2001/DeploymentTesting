7a564e0c653064ff6e1f12e994189949b61cc8f4
Ayden-Owens <aydeno@email.sc.edu>
2023-11-26 18:41:05 -0500

    dietary restrictions page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from './Home'
 import Login from './Login'
 import Register from './Register'
 import Profile from './Profile'
+import DietaryRestrictions from './DietaryRestrictions'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='/register' Component={Register}/>
           <Route path='/Home' Component={Home}/>
           <Route path='/Profile' Component={Profile}/>
+          <Route path='/DietaryRestrictions' Component={DietaryRestrictions}/>
         </Routes>
       </div>
     </Router>
