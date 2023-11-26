0d005d0fef59eca813d08504f82d7254b049e4d4
Valerie Duffey <69978259+valerieduffey@users.noreply.github.com>
2023-11-26 18:53:07 -0500

    Imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-//import "./styles.css";
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Home'
