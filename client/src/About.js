4e3ca24b5de1d12ad844af2e45aa357db174c1aa
Valerie Duffey <69978259+valerieduffey@users.noreply.github.com>
2023-11-26 19:15:38 -0500

    styling

--- client/src/About.js
+++ client/src/About.js
@@ -5,8 +5,9 @@ function About() {
     return (
         <div>
             <div>
-                <h1>About Us</h1>
+                <h1 style = {{background: 'white', size:'30px'}}>About Us</h1>
                 <h1>At WhatToCook, our mission is to simplify your cooking experiences. Input your fridge's ingredients, and our recipe generator will create delicious meals just for you. Plus, our price comparer helps you find the best deals on ingredients, making your kitchen adventures both tasty and budget-friendly.</h1>
+                <h1 style = {{background: 'white', size:'30px'}}> Meet the Creators </h1>
             </div>
             <div style = {{background: 'white', margin:'10px'}}>
                 <h2> Valerie Duffey</h2>
