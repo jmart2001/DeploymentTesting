17524064e1ca7781dfbc975dbd5eaf999e5ec287
Valerie Duffey <69978259+valerieduffey@users.noreply.github.com>
2023-11-27 10:40:59 -0500

    format

--- client/src/Login.js
+++ client/src/Login.js
@@ -53,7 +53,7 @@ const Login = () => {
     // }, [])
 
     return (
-        <div className="login">
+        <div className="login" style={{textAlign:'center',fontFamily:'cursive',}}>
             <h1>Login</h1>
             <input 
                 type="text" 
@@ -70,9 +70,14 @@ const Login = () => {
                 }}
             />
             <button onClick={handleLogin}>Login</button>
-            <div>
+            {/* <div>
                 <Link to="/register">Register</Link>
-            </div>
+            </div> */}
+            <Link to="/register">
+                <button style={{fontFamily: 'cursive',fontSize: '30px'}}>
+                    Register
+                </button>
+            </Link>
             <p className="message">{loginStatus}</p>
         </div>       
     )
