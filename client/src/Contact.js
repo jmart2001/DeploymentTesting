5fba6b1e9658110519780716e272767972cf435f
Valerie Duffey <69978259+valerieduffey@users.noreply.github.com>
2023-11-26 20:04:02 -0500

    ContactBaseCode

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -1,15 +1,65 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-function Contact() {
-    return (
-        <div>
-            <div>
-                <h1>Contact Us</h1>
-            </div>
-        </div>
-    )
-}
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
 
-export default Contact
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Replace 'YOUR_API_ENDPOINT' with the actual endpoint where your backend handles email sending
+      await axios.post('YOUR_API_ENDPOINT', formData);
+
+      // Clear the form after successful submission
+      setFormData({ name: '', email: '', message: '' });
+
+      console.log('Email sent successfully');
+    } catch (error) {
+      console.error('Error sending email', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name:</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="message">Message:</label>
+      <textarea
+        id="message"
+        name="message"
+        value={formData.message}
+        onChange={handleChange}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Contact;
