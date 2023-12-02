0b5e0f49c70180613a6eae21be4c0a5a6d8c3cdd
Justin Matthew Martin <justinmmartin01@gmail.com>
2023-12-02 14:25:54 -0500

    comparing

--- server/scripts/data/Pork/searching.js
+++ server/scripts/data/Pork/searching.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import fs from 'fs';
 
 // Read the JSON file
@@ -30,36 +29,3 @@ const modifiedRecipesJSON = JSON.stringify(modifiedRecipes, null, 2);
 fs.writeFileSync('modified_pork_recipes.json', modifiedRecipesJSON);
 
 console.log('Modified recipes have been saved to modified_pork_recipes.json');
-=======
-import fs from 'fs';
-
-// Read the JSON file
-const rawData = fs.readFileSync('pork_recipies.json');
-const recipes = JSON.parse(rawData);
-
-// Prepare an array to store the modified recipe details
-const modifiedRecipes = [];
-
-// Modify and store details for each recipe
-recipes.forEach(recipe => {
-    const modifiedRecipe = {
-        recipeName: recipe.label,
-        ingredients: recipe.ingredients.map(ingredient => ({
-            food: ingredient.food,
-            quantity: ingredient.quantity,
-            measure: ingredient.measure
-        })),
-        healthLabels: recipe.healthLabels.map(label => label.replace(/-/g, '')),
-        totalTime: recipe.totalTime,
-        instructions: recipe.instructions
-    };
-
-    modifiedRecipes.push(modifiedRecipe);
-});
-
-// Write the modified results to a file
-const modifiedRecipesJSON = JSON.stringify(modifiedRecipes, null, 2);
-fs.writeFileSync('modified_pork_recipes.json', modifiedRecipesJSON);
-
-console.log('Modified recipes have been saved to modified_pork_recipes.json');
->>>>>>> 297f26138db94fb0a1e2dcacedd50018aa4707a9
